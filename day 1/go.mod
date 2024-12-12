108c1275d8bd08df67b5beff89e675fe49d3cfbe
chavocito <francbota94@gmail.com>
2024-12-12 17:55:28 +0000

    distance challenge

---
+++ day 1/go.mod
@@ -0,0 +1,3 @@
+module github.com/chavocito/aoc
+
+go 1.21.6
---
+++ day 1/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const input = "input.txt"
+
+func main() {
+	file, err := os.Open(input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	leftSlice, rightSlice, err := separateInputs(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	slices.Sort(leftSlice)
+	slices.Sort(rightSlice)
+
+	fmt.Println(accumulator(leftSlice, rightSlice))
+}
+
+func accumulator(left []int, right []int) int {
+	total := 0
+	for i := 0; i < len(left); i++ {
+		diff := int(math.Abs(float64(left[i] - right[i])))
+		total += diff
+	}
+	return total
+}
+
+func separateInputs(file *os.File) ([]int, []int, error) {
+	// Create arrays to hold the values
+	var err error
+	var leftValues []int
+	var rightValues []int
+
+	// Read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+		if len(parts) == 2 {
+			left, err1 := strconv.Atoi(parts[0])
+			right, err2 := strconv.Atoi(parts[1])
+			if err1 == nil && err2 == nil {
+				leftValues = append(leftValues, left)
+				rightValues = append(rightValues, right)
+			}
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, nil, err
+	}
+
+	return leftValues, rightValues, nil
+}
