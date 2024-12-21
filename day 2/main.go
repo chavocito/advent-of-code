8a0f09308fbb454fa2a9a16b81e3575b43c96bf4
chavocito <francbota94@gmail.com>
2024-12-21 23:29:54 +0000

    update main to add countsafepath func

--- day 2/main.go
+++ day 2/main.go
@@ -17,16 +17,32 @@ func main() {
 		fmt.Println("Error reading file")
 		return
 	}
-	fmt.Println("Safe Combinations: ", input)
+	count := countSafePaths(input)
+	fmt.Println("Number of safe paths:", count)
+}
+
+func countSafePaths(array [][]int) int {
+	var count int = 0
+	for _, levels := range array {
+		if isSafe(levels) {
+			count++
+		}
+	}
+	return count
 }
 
 func isSafe(levels []int) bool {
+	if len(levels) < 2 {
+		return true
+	}
+
+	increasing := levels[0] < levels[1]
 	for i := 0; i < len(levels)-1; i++ {
 		adjacencyDiff := math.Abs(float64(levels[i] - levels[i+1]))
 		if adjacencyDiff < 1 || adjacencyDiff > 3 {
 			return false
 		}
-		if levels[i] == levels[i+1] {
+		if (increasing && levels[i] >= levels[i+1]) || (!increasing && levels[i] <= levels[i+1]) {
 			return false
 		}
 	}
@@ -49,14 +65,16 @@ func separateInputs(filePath string) ([][]int, error) {
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Fields(line)
+		var nums []int
 		for _, part := range parts {
 			vals, err := strconv.Atoi(part)
 			if err != nil {
 				fmt.Println("Error converting string to int:", err)
 				return nil, err
 			}
-			collections = append(collections, []int{vals})
+			nums = append(nums, vals)
 		}
+		collections = append(collections, nums)
 	}
 	if err = scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
